4719c7e8f48cddc09ae8d4a93e24ceb156783f79
Jason Flannick <flannick@broadinstitute.org>
2019-09-19 13:57:09 -0400

    Updated to use q-values

--- python-flask-server/swagger_server/controllers/transformer_controller.py
+++ python-flask-server/swagger_server/controllers/transformer_controller.py
@@ -11,14 +11,15 @@ from swagger_server.models.attribute import Attribute
 
 #REQUIREMENTS
 import scipy.stats
+from numpy import array, empty
 #available at http://software.broadinstitute.org/gsea/downloads.jsp
 msigdb_gmt_file='dat/c2.all.v7.0.entrez.gmt'
 
 #END REQUIREMENTS
 
-valid_controls = ['max p-value']
-default_control_values = {'max p-value': 1e-5}
-default_control_types = {'max p-value': 'double'}
+valid_controls = ['max p-value', 'max q-value']
+default_control_values = {'max p-value': 1e-5, 'max q-value': 0.05}
+default_control_types = {'max p-value': 'double', 'max q-value': 'double'}
 
 def get_control(controls, control):
     value = controls[control] if control in controls else default_control_values[control]
@@ -41,6 +42,38 @@ def entrez_gene_id(gene: GeneInfo):
         else:
             return gene.identifiers.entrez
     return None
+
+def correct_pvalues_for_multiple_testing(pvalues, correction_type = "Benjamini-Hochberg"):                
+    """                                                                                                   
+    consistent with R - print correct_pvalues_for_multiple_testing([0.0, 0.01, 0.029, 0.03, 0.031, 0.05, 0.069, 0.07, 0.071, 0.09, 0.1]) 
+    """
+    pvalues = array(pvalues) 
+    n = int(pvalues.shape[0])                                                                           
+    new_pvalues = empty(n)
+    if correction_type == "Bonferroni":                                                                   
+        new_pvalues = n * pvalues
+    elif correction_type == "Bonferroni-Holm":                                                            
+        values = [ (pvalue, i) for i, pvalue in enumerate(pvalues) ]                                      
+        values.sort()
+        for rank, vals in enumerate(values):                                                              
+            pvalue, i = vals
+            new_pvalues[i] = (n-rank) * pvalue                                                            
+    elif correction_type == "Benjamini-Hochberg":                                                         
+        values = [ (pvalue, i) for i, pvalue in enumerate(pvalues) ]                                      
+        values.sort()
+        values.reverse()                                                                                  
+        new_values = []
+        for i, vals in enumerate(values):                                                                 
+            rank = n - i
+            pvalue, index = vals                                                                          
+            new_values.append((n/rank) * pvalue)                                                          
+        for i in range(0, int(n)-1):  
+            if new_values[i] < new_values[i+1]:                                                           
+                new_values[i+1] = new_values[i]                                                           
+        for i, vals in enumerate(values):
+            pvalue, index = vals
+            new_pvalues[index] = new_values[i]
+    return new_pvalues
 
 def transform_post(query):  # noqa: E501
     """transform_post
@@ -91,20 +124,31 @@ def transform_post(query):  # noqa: E501
     M = len(all_gene_set_gene_ids)
 
     gene_set_pvalues = {}
+    gene_set_qvalues = {}
     gene_set_odds_ratios = {}
+    all_pvalues = []
+    all_gene_set_ids = []
     for gene_set_id in gene_set_y_gene_list_y:
         gene_set_n_gene_list_n[gene_set_id] = M - gene_set_y_gene_list_y[gene_set_id] - gene_set_y_gene_list_n[gene_set_id] - gene_set_n_gene_list_y[gene_set_id]
 
         table = [[gene_set_y_gene_list_y[gene_set_id], gene_set_y_gene_list_n[gene_set_id]], [gene_set_n_gene_list_y[gene_set_id], gene_set_n_gene_list_n[gene_set_id]]]
         odds_ratio, pvalue = scipy.stats.fisher_exact(table)
 
+        all_pvalues.append(pvalue)
+        all_gene_set_ids.append(gene_set_id)
+
         if pvalue < get_control(controls, 'max p-value'):
             gene_set_pvalues[gene_set_id] = pvalue
             gene_set_odds_ratios[gene_set_id] = odds_ratio
 
+    all_qvalues = correct_pvalues_for_multiple_testing(all_pvalues, correction_type="Benjamini-Hochberg")
+    for i, gene_set_id in enumerate(all_gene_set_ids):
+        if gene_set_id in gene_set_pvalues and all_qvalues[i] < get_control(controls, 'max q-value'):
+            gene_set_qvalues[gene_set_id] = all_qvalues[i]
+
     final_genes = {}
     final_gene_list = []
-    for gene_set_id in sorted(gene_set_pvalues.keys(), key=lambda x: gene_set_pvalues[x]):
+    for gene_set_id in sorted(gene_set_qvalues.keys(), key=lambda x: gene_set_qvalues[x]):
         for gene_id in gene_set_gene_list_gene_ids[gene_set_id]:
             gene_entrez_id = "NCBIGene:%s" % gene_id
             if gene_id in genes and gene_entrez_id not in final_genes:
@@ -120,6 +164,12 @@ def transform_post(query):  # noqa: E501
                       Attribute(
                         name = 'p-value',
                         value = gene_set_pvalues[gene_set_id],
+                        source = 'Hypergeometric enrichment filter'
+                      ))
+                final_genes[gene_entrez_id].attributes.append(
+                      Attribute(
+                        name = 'q-value',
+                        value = gene_set_qvalues[gene_set_id],
                         source = 'Hypergeometric enrichment filter'
                       ))
                 final_genes[gene_entrez_id].attributes.append(
